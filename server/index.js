87e43f26ebebd1716567e560dbf92be0c7615848
AlecCross <cross2472@gmail.com>
2022-01-07 12:50:37 +0200

    first server start(test)

--- server/index.js
+++ server/index.js
@@ -1,1 +1,18 @@
-console.log("MERN - FullStack разработка облачного")
+const express = require("express")
+const mongoose = require("mongoose")
+const config = require("config")
+
+const app = express()
+const PORT = config.get('serverPort')
+const start = () => {
+    try {
+        
+        app.listen(PORT, ()=>{
+            console.log(`Server started on port ${PORT}`)
+        })
+    } catch (error) {
+        
+    }
+}
+
+start()
